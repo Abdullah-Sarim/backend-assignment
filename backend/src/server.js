c3766375568d6e6206fba753eaf7dff281c55472
Sarim Abdullah <abdullahsarim1100@gmail.com>
2026-04-16 21:42:32 +0530

    Allow all origins for CORS to fix connection issue

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,18 +17,7 @@ import User from './models/User.js';
 
 const app = express();
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    const allowedOrigins = process.env.CLIENT_URL ? process.env.CLIENT_URL.split(',') : ['http://localhost:5173', 'http://localhost:3000'];
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
-};
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 
